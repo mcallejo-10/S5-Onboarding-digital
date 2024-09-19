64fcf6b6798e3f5b5a0477b39989c514ca3b1812
Miranda Callejón <mirandacallejonh@gmail.com>
2024-09-19 12:46:48 +0200

    afegint imatges

--- onBoarding/src/app/steps.service.ts
+++ onBoarding/src/app/steps.service.ts
@@ -10,21 +10,21 @@ export class StepsService {
       title: 'Dedica moltes hores',
       description:
         'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-      img: '',
+      img: '../assets/time_managment.svg',
       bgcolor: '',
     },
     {
       title: 'Programa projectes propis:',
       description:
         'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
-      img: '',
+      img: 'onBoarding/src/app/assets/programming.svg',
       bgcolor: '',
     },
     {
       title: 'Procura descansar:',
       description:
         "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-      img: '',
+      img: 'onBoarding/src/app/assets/time_managment.svg',
       bgcolor: '',
     },
   ];
